d78364dad9afd62f03db5061b0912fb65a6bf228
Karpich Dmitry <karpich@gollard.ru>
2018-01-15 18:28:46 +0300

    * rebuild examples

--- example/index.js
+++ example/index.js
@@ -4,19 +4,25 @@ var _ = require('..');
 
 // important turn on Debugger BEFORE load layers
 _.Debugger.setMode(true); // main start point aka 'index'
+
 _.Auditor.setMode(true);
 
 // load layers, in ANY orders
 require('./services_layer');
+
 require('./shared_layer');
 
+
 var antiniteSys = new _.System('mainSystem'); // create system object to access any exported actions (system do 'require *', kind of)
-var res = antiniteSys.execute('service', 'FooService', 'doFoo', 'here'); // system may call any service (BUT only if service rights allow it)
 
-console.log(res); // `here its bar and foo and its bar`
+antiniteSys.onReady().then(function () {
+  var res = antiniteSys.execute('service', 'FooService', 'doFoo', 'here'); // system may call any service (BUT only if service rights allow it)
 
-console.log('-Auditor data-');
-console.log(_.Auditor.getData());
+  console.log(res); // `here its bar and foo and its bar`
 
-console.log('-Debugger data-');
-console.log(_.Debugger.getData());
+  console.log('-Auditor data-');
+  console.log(_.Auditor.getData());
+
+  console.log('-Debugger data-');
+  console.log(_.Debugger.getData());
+});
